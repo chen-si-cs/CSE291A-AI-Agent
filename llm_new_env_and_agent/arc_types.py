88fa4ae115b7c232a507b8562f2eb89fc7fb83b2
Jiarui Chen <jrchenjiarui@163.com>
2026-03-09 13:50:57 -0700

    Add new env and agent for LLM
    1. add new env and llm agent
    2. fix bug in distractor notebook about lambda error exception
    3. add logs for distractor 10 (also 2 with 40 logs)

---
+++ llm_new_env_and_agent/arc_types.py
@@ -0,0 +1,17 @@
+from typing import Any, Callable, Container, FrozenSet, Tuple as Tuple
+Boolean = bool
+Integer = int
+Numerical = Any
+IntegerTuple = Tuple
+IntegerSet = Any
+ContainerContainer = Any
+Grid = Any
+Cell = Any
+Object = Any
+Objects = Any
+Indices = Any
+IndicesSet = Any
+Patch = Any
+Element = Any
+Piece = Any
+TupleTuple = Any
